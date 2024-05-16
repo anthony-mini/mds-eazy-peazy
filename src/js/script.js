6eec142b34841ca7531d97792ae90e809c3f6141
anthony-mini <anthony-mini@outlook.fr>
2024-05-16 12:03:12 +0200

    feat: Add popup functionality for registration page

--- src/js/script.js
+++ src/js/script.js
@@ -46,3 +46,13 @@ document
   .addEventListener('click', function () {
     redirectToPage('./sign-up.html');
   });
+
+function showPopup() {
+  document.getElementById('popup').style.display = 'block';
+  document.getElementById('overlay').style.display = 'block';
+}
+
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+  document.getElementById('overlay').style.display = 'none';
+}
